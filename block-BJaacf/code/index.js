29779f5fdbe913450bd16da46db8a8320120b3c9
chandresh dave <chandreshd89@gmail.com>
2021-11-09 10:15:24 +0530

    ASSIGNMENT

--- block-BJaacf/code/index.js
+++ block-BJaacf/code/index.js
@@ -1,15 +1,27 @@
 // 1. Create an array named numbers and store 5 number values in it
-
+let numbers = [1, 3, 6, 7, 8];
 // 2. Calculate the sum of array items and print it to the console using console.log()
-
+let sum = 0;
+for (i = 0; i < numbers.length; i++) {
+  sum += numbers.length;
+  console.log(sum);
+}
 // 3. Calculate the average of array items and print it to the console using console.log()
-
+let sum = 1;
+for (i = 0; i < numbers.length; i++) {
+  sum *= numbers.length;
+  console.log(sum);
+}
 // 4. Find the highest number in the array and print it to the console using console.log()
-
+console.log(numbers[4]);
 // 5. Find the lowest number in the array and print it to the console using console.log()
+console.log(numbers[0]);
 
 // 6. Find the even numbers in the array and print them to the console using console.log()
-
+let numbers = [1, 3, 6, 7, 8];
+if (numbers % 2 === 0) {
+  console.log(numbers);
+}
 // 7. Find the odd numbers in the array and print them to the console using console.log()
 
 // 8. Find the numbers in the array that is divisible by 5 and print them to the console using console.log()
